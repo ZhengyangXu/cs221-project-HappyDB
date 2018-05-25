e7a33d2280a633fdabaf7db20d9c524bf882d052
Qiqi Ren <qiqi.ren@gmail.com>
2018-05-25 14:31:47 -0400

    Added svm

--- classification.py
+++ classification.py
@@ -2,6 +2,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
 import csv
 import numpy as np
 import re
@@ -10,10 +11,17 @@ filename = "data/data.csv"
 def run_naive_bayes(data, labels, test_data, test_labels):
 	# get data from get_features
 	clf = MultinomialNB().fit(data, labels)
-	predicted = clf.predict(test_data)
-	print(np.mean(predicted == test_labels))
+	predicted = clf.predict(data) # change to test_data intead of data when ready for testing
+	print("Naive Bayes training accuracy: ")
+	print(np.mean(predicted == labels))
 
-def get_features():
+def run_svm(data, labels, test_data, test_labels):
+	clf = SGDClassifier(max_iter = 10).fit(data, labels)
+	predicted = clf.predict(data) # change to test_data intead of data when ready for testing
+	print("SVM training accuracy: ")
+	print(np.mean(predicted == labels))
+
+def get_features_bag_of_words():
 	with open(filename, 'rU') as csvfile:
 		reader = csv.DictReader(csvfile)
 		text_data = []
@@ -54,6 +62,10 @@ def get_features():
 				parent_labels.append(0)
 	count_vect = CountVectorizer()
 	X_counts = count_vect.fit_transform(text_data)
+
+	# tfidf_transformer = TfidfTransformer()
+	# X_counts = tfidf_transformer.fit_transform(X_counts) # for ignoring length of text and reducing weight of common words
+	
 	training_size = int(X_counts.shape[0]*0.8) # 80:20 split for train/test data
 	X_train_counts = X_counts[:training_size, :] 
 	age_bucket_labels_train = age_bucket_labels[:training_size]
@@ -68,17 +80,18 @@ def get_features():
 	parent_labels_test = parent_labels[training_size:]
 	y_dict_test = {'age': age_bucket_labels_test, 'marital': marital_labels_test, 'gender': gender_labels_test, 'parenthood': parent_labels_test}
 
-	# tfidf_transformer = TfidfTransformer()
-	# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts) for ignoring length of text and reducing weight of common words, try later?
 	return X_train_counts, y_dict_train, X_test_counts, y_dict_test
 
 def main():
-	x_train, y_dict_train, x_test, y_dict_test = get_features()
-	# run_naive_bayes(x_train, y_dict['gender'], x_test, y_dict_test['gender'])
+	x_train, y_dict_train, x_test, y_dict_test = get_features_bag_of_words()
 	run_naive_bayes(x_train, y_dict_train['marital'], x_test, y_dict_test['marital'])
 	run_naive_bayes(x_train, y_dict_train['gender'], x_test, y_dict_test['gender'])
 	run_naive_bayes(x_train, y_dict_train['parenthood'], x_test, y_dict_test['parenthood'])
 	run_naive_bayes(x_train, y_dict_train['age'], x_test, y_dict_test['age'])
+	run_svm(x_train, y_dict_train['marital'], x_test, y_dict_test['marital'])
+	run_svm(x_train, y_dict_train['gender'], x_test, y_dict_test['gender'])
+	run_svm(x_train, y_dict_train['parenthood'], x_test, y_dict_test['parenthood'])
+	run_svm(x_train, y_dict_train['age'], x_test, y_dict_test['age'])
 
 if __name__ == '__main__':
 	main()
